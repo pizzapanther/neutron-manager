1a63b3f8f80ba8aa7078d666291776ac10ee02c9
Paul Bailey <paul@neutron.studio>
2022-03-25 15:56:27 -0500

    admin setup

--- nmanage/resources/admin.py
+++ nmanage/resources/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from nmanage.resources.models import Resource, Permission
+
+
+class PermissionInline(admin.TabularInline):
+  model = Permission
+  raw_id_fields = ('user',)
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'rid', 'rtype', '_permissions', 'modified')
+  list_filter = ('rtype', 'created', 'modified')
+  date_hierarchy = 'modified'
+  search_fields = ('name', 'rid')
+
+  inlines = (PermissionInline,)
+
+  def _permissions(self, obj):
+    return obj.permission_set.all().count()

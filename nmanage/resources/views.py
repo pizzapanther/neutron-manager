2370bab934cf9b4dacc3f845a92db8c319bcdf05
Paul Bailey <paul@neutron.studio>
2022-03-30 16:11:47 -0500

    resource page

--- nmanage/resources/views.py
+++ nmanage/resources/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.template.response import TemplateResponse
 
-# Create your views here.
+from nmanage.resources.models import Resource
+
+
+@login_required
+def my_resources(request):
+  resources = Resource.objects.filter(permission__user=request.user).exclude(account__isnull=True)
+  paginator = Paginator(resources, 50)
+
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  context = {
+      'page_obj': page_obj
+  }
+  return TemplateResponse(request, 'resources/list.html', context)
--- nmanage/urls.py
+++ nmanage/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import nmanage.resources.views as resource_views
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path('resources/list/', resource_views.my_resources),
 ]
